a6df090eb5c03ae2baba0fe358d90797048865c8
KarolKwcN <karol.kwiecien91@gmail.com>
2018-10-17 21:47:24 +0200

    Dziennik (koniec odcinka)

---
+++ Dzienniczek/Dziennik.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Dzienniczek
+{
+    class Dziennik
+    {
+        // Stan (zmienne - pola)
+
+        List<float> oceny;
+
+        // Zachowania 
+        public void DodawanieOcen(float ocena)
+        {
+            oceny.Add(ocena);
+        }
+
+        public float LiczenieSredniej()
+        {
+            float suma = 0, srednia = 0;
+
+            foreach(var ocena in oceny)
+            {
+                suma = suma + ocena;
+            }
+
+            srednia = suma / oceny.Count();
+            return srednia;
+
+        }
+
+        public float PobierzMaksymalnaOcene()
+        {
+            return oceny.Max();
+        }
+
+        public float PobierzNajnizszaOcene()
+        {
+            return oceny.Min();
+        }
+    }
+}
