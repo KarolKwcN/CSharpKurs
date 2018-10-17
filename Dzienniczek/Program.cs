678d388643a3a45fd1a547fca614cd5777a1c689
KarolKwcN <karol.kwiecien91@gmail.com>
2018-10-17 23:11:11 +0200

    Dzienniczek ( wprowadzanie danych przez uzytkownika)

--- Dzienniczek/Program.cs
+++ Dzienniczek/Program.cs
@@ -11,13 +11,10 @@ namespace Dzienniczek
         static void Main(string[] args)
         {
             Dziennik dzienniczek = new Dziennik();
-
+            /*
             dzienniczek.DodawanieOcen(5);
             dzienniczek.DodawanieOcen(7.6f);
             dzienniczek.DodawanieOcen(3.4f);
-
-            
-
             float srednia = dzienniczek.LiczenieSredniej();
             float maks = dzienniczek.PobierzMaksymalnaOcene();
             float min = dzienniczek.PobierzNajnizszaOcene();
@@ -25,8 +22,33 @@ namespace Dzienniczek
             Console.WriteLine(srednia);
             Console.WriteLine(maks);
             Console.WriteLine(min);
+            */
+        
+            for(  ; ; )
+            {
+                Console.WriteLine("Podaj ocene: ");
+
+                float ocenyy;
+                bool wynik = float.TryParse(Console.ReadLine(), out ocenyy); //sprawdzamy czy wprowadzana dana to liczba
+
+                if (ocenyy == 11)
+                {
+                    break;
+                }
+
+                if (wynik)
+                {
+                dzienniczek.DodawanieOcen(ocenyy);
+                }
+
+                
 
 
+            }
+
+            Console.WriteLine("Srednia Twoich ocen to : "+ dzienniczek.LiczenieSredniej());
+            Console.WriteLine("Mkasymalna ocena to : " + dzienniczek.PobierzMaksymalnaOcene());
+            Console.WriteLine("minimalna ocena to : " + dzienniczek.PobierzNajnizszaOcene());
             Console.ReadKey();
         }
     }
